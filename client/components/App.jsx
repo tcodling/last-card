fea4102b3876789237cb1e7ed1072dc70c4825c0
tcodling <thomas.codling@outlook.com>
2020-05-23 11:17:23 +1200

    add shuffle

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,14 +3,18 @@ import React from 'react'
 import Play from './Play'
 import Deck from './Deck'
 
-const App = () => {
-  return (
-    <>
-    <h1>Last Card</h1>
-    <Play />
-    <Deck />
-    </>
-  )
+import cards from '../cards.json'
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+      <h1>Last Card</h1>
+      <Play />
+      <Deck cards={cards} />
+      </>
+    )
+  }
 }
 
 export default App
--- client/components/Deck.jsx
+++ client/components/Deck.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const Deck = () => {
-  return (
-    <div id='deck'></div>
-  )
+class Deck extends React.Component {
+  state = {
+    cards: this.props.cards
+  }
+
+  shuffle = () => {
+    this.setState({
+      cards: this.state.cards.sort(() => Math.random() - 0.5)
+    })
+  }
+
+  render() {
+    return (
+      <div id='deck' onClick={this.shuffle}></div>
+    )
+  }
 }
 
 export default Deck
