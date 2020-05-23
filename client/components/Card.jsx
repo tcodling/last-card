2ff84ec9211ed05e4731f677afc5b1e95591801d
tcodling <thomas.codling@outlook.com>
2020-05-23 15:09:32 +1200

    add symbols to cards

--- client/components/Card.jsx
+++ client/components/Card.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
 const Card = (props) => {
+  let image
+  let suit = props.card.suit
+  if (suit === 'hearts') {
+    image = '/images/heart.svg'
+  } else if (suit === 'spades') {
+    image = '/images/spade.svg'
+  } else if (suit === 'diamonds') {
+    image = '/images/diamond.svg'
+  } else if (suit === 'clubs') {
+    image = '/images/club.svg'
+  }
+
   return (
     <div id={props.id} onClick={props.playCard} className='card'>
-      <p>{props.card.value} {props.card.suit}</p>
+      <p className='leftValue'>{props.card.value}</p>
+      <img className='suit' src={image} />
+      <p className='rightValue'>{props.card.value}</p>
     </div>
   )
 }
