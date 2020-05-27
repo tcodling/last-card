3ffb149d14ef27635b9ea161f40d1568d48edc6a
tcodling <thomas.codling@outlook.com>
2020-05-27 15:44:00 +1200

    add more opponent funcitonality

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
-import Play from './Play'
-import Deck from './Deck'
-import Hand from './Hand'
-import Style from './Style'
-import OppHand from './OppHand'
+import Play from "./Play";
+import Deck from "./Deck";
+import Hand from "./Hand";
+import Style from "./Style";
+import OppHand from "./OppHand";
 
-import cards from '../cards.json'
+import cards from "../cards.json";
 
 class App extends React.Component {
   state = {
@@ -14,145 +14,165 @@ class App extends React.Component {
     hand: [],
     oppHand: [],
     play: [],
-    buttonText: '',
+    buttonText: "",
     draw: false,
-    style: 'classic'
-  }
+    style: "classic",
+  };
 
   componentDidMount = () => {
-    this.shuffle()
-  }
+    this.shuffle();
+  };
 
   swapStyle = () => {
-    if (event.target.innerHTML === 'Pixel') {
-      document.getElementById('style').setAttribute('href', '/pixel.css')
-    } else if (event.target.innerHTML === 'Classic') {
-      document.getElementById('style').setAttribute('href', '/main.css')
+    if (event.target.innerHTML === "Pixel") {
+      document.getElementById("style").setAttribute("href", "/pixel.css");
+    } else if (event.target.innerHTML === "Classic") {
+      document.getElementById("style").setAttribute("href", "/main.css");
     }
-  }
+  };
 
   shuffle = () => {
     this.setState({
-      deck: this.state.deck.sort(() => Math.random() - 0.5)
-    })
-  }
+      deck: this.state.deck.sort(() => Math.random() - 0.5),
+    });
+  };
 
   changeButton = (i) => {
-    if (i === 'error') {
+    if (i === "error") {
       this.setState({
-        buttonText: 'You can not place that card'
-      })
-    } else if (i === 'success') {
+        buttonText: "You can not place that card",
+      });
+    } else if (i === "success") {
       this.setState({
-        buttonText: 'Good Job!'
-      })
-    } else if (i === 'start') {
+        buttonText: "Good Job!",
+      });
+    } else if (i === "start") {
       this.setState({
-        buttonText: 'Lets go!'
-      })
-    } else if (i === 'draw') {
+        buttonText: "Lets go!",
+      });
+    } else if (i === "draw") {
       this.setState({
-        buttonText: 'Oh no'
-      })
-    } else if (i === 'win') {
+        buttonText: "Oh no",
+      });
+    } else if (i === "win") {
       this.setState({
-        buttonText: 'You win! Start over?'
-      })
+        buttonText: "You win! Start over?",
+      });
     }
-  }
+  };
 
   draw = () => {
-    let currentHand = this.state.hand
-    currentHand.push(this.state.deck[0])
+    let currentHand = this.state.hand;
+    currentHand.push(this.state.deck[0]);
     this.setState({
       hand: currentHand,
-      deck: this.state.deck.slice(1)
-    })
-    this.changeButton('draw')
-  }
+      deck: this.state.deck.slice(1),
+    });
+    this.changeButton("draw");
+    this.oppPlay();
+  };
+
+  oppDraw = () => {
+    setTimeout(() => {
+      let currentOppHand = this.state.oppHand;
+      currentOppHand.push(this.state.deck[0]);
+      this.setState({
+        oppHand: currentOppHand,
+        deck: this.state.deck.slice(1),
+      });
+    }, 1000);
+  };
 
   oppPlay = () => {
-    let currentPlay = this.state.play
+    let currentPlay = this.state.play;
     for (let i in this.state.oppHand) {
-      let card = this.state.oppHand[i]
-      console.log(card)
-      if (card.suit === currentPlay[0].suit || card.value === currentPlay[0].value) {
-      setTimeout(() => {
-        currentPlay.unshift(card)
-        let newOppHand = this.state.oppHand
-        newOppHand.splice(newOppHand.indexOf(card), 1)
-        this.setState({
-          play: currentPlay,
-          oppHand: newOppHand
-        })}, 1000)
-        return
+      let card = this.state.oppHand[i];
+      console.log(card);
+      if (
+        card.suit === currentPlay[0].suit ||
+        card.value === currentPlay[0].value
+      ) {
+        setTimeout(() => {
+          currentPlay.unshift(card);
+          let newOppHand = this.state.oppHand;
+          newOppHand.splice(newOppHand.indexOf(card), 1);
+          this.setState({
+            play: currentPlay,
+            oppHand: newOppHand,
+          });
+        }, 1000);
+        return;
       }
-  }
-  }
+    }
+    this.oppDraw();
+  };
 
   play = (event) => {
-    let currentPlay = this.state.play
-    let card = this.state.hand[event.target.id]
-    if (currentPlay[0] === undefined || card.suit === currentPlay[0].suit || card.value === currentPlay[0].value) {
-      currentPlay.unshift(card)
-      let newHand = this.state.hand
-      newHand.splice(newHand.indexOf(card), 1)
+    let currentPlay = this.state.play;
+    let card = this.state.hand[event.target.id];
+    if (
+      currentPlay[0] === undefined ||
+      card.suit === currentPlay[0].suit ||
+      card.value === currentPlay[0].value
+    ) {
+      currentPlay.unshift(card);
+      let newHand = this.state.hand;
+      newHand.splice(newHand.indexOf(card), 1);
       this.setState({
         play: currentPlay,
-        hand: newHand
-      })
-      this.changeButton('success')
+        hand: newHand,
+      });
+      this.changeButton("success");
       if (this.state.hand.length === 0) {
-        this.changeButton('win')
+        this.changeButton("win");
       }
 
-      this.oppPlay()
+      this.oppPlay();
     } else {
-      this.changeButton('error')
+      this.changeButton("error");
     }
-  }
+  };
 
   startGame = () => {
-    let hand = (this.state.deck.slice(0, 7))
-    let oppHand = (this.state.deck.slice(7, 14))
+    let hand = this.state.deck.slice(0, 7);
+    let oppHand = this.state.deck.slice(7, 14);
     this.setState({
       hand: hand,
       oppHand: oppHand,
       deck: this.state.deck.slice(14),
-      draw: true
-    })
-    this.changeButton('start')
-  }
+      draw: true,
+    });
+    this.changeButton("start");
+  };
 
   render() {
     return (
       <>
-      <h1 className='bounce-in-left' id='heading'>L
-        <span className='red'>a
-        </span>s
-        <span className='red'>t
-        </span> C
-        <span className='red'>a
-        </span>r
-        <span className='red'>d
-        </span></h1>
-      <Style changeStyle={this.swapStyle} />
-      <div id='wood'>
-        <OppHand oppHand={this.state.oppHand} />
-        <div id='board'>
-          <Play played={this.state.play[0]} />
-          <Deck cards={this.state.deck} />
-          <div id='helper'>
+        <h1 className="bounce-in-left" id="heading">
+          L<span className="red">a</span>s<span className="red">t</span> C
+          <span className="red">a</span>r<span className="red">d</span>
+        </h1>
+        <Style changeStyle={this.swapStyle} />
+        <div id="wood">
+          <OppHand oppHand={this.state.oppHand} />
+          <div id="board">
+            <Play played={this.state.play[0]} />
+            <Deck cards={this.state.deck} />
+            {/* <div id='helper'>
             <h2>{this.state.buttonText}</h2>
+          </div> */}
           </div>
+          <Hand playCard={this.play} hand={this.state.hand} />
         </div>
-        <Hand playCard={this.play} hand={this.state.hand} />
-      </div>
-      {!this.state.draw ? <button onClick={this.startGame}>Start Game</button> : <></>}
-      {this.state.draw ? <button onClick={this.draw}>Draw</button> : <></>}
+        {!this.state.draw ? (
+          <button onClick={this.startGame}>Start Game</button>
+        ) : (
+          <></>
+        )}
+        {this.state.draw ? <button onClick={this.draw}>Draw</button> : <></>}
       </>
-    )
+    );
   }
 }
 
-export default App
+export default App;
